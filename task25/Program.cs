27418b67e19d0a86d518e6a41ef9a17917c4c7bc
Nick Gir <mbiko@ya.ru>
2023-03-11 16:36:26 +0300

    add task 25

---
+++ task25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double GetNumberDegree (double number, double degree)
+{
+    double NumberDegree = Math.Pow(number, degree);
+
+    return NumberDegree;
+}
+
+
+Console.WriteLine("Введите число A: ");
+double A = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+double B = double.Parse(Console.ReadLine());
+
+double NumDegree = GetNumberDegree(A, B);
+
+Console.WriteLine($"{A} возвести в натуральную степень {B} получаем {NumDegree}");
