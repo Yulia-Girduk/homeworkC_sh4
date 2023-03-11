e08501431daee366153c36667ec38b29c0204cbf
Nick Gir <mbiko@ya.ru>
2023-03-11 16:37:47 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void GetWriteArray (int[] arrayNum)
+{
+    Console.Write($"[");
+
+    for (int i = 0; i < arrayNum.Length; i++)
+    {
+      Console.Write($"{arrayNum[i]} "); 
+    }
+
+    Console.Write($"]");
+}
+
+int[] GetRandome (int[] arrayNumber, int numStart, int numEnd)
+{
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+      arrayNumber[i] = new Random().Next(numStart, numEnd+1);  
+    }
+    return arrayNumber;
+}
+
+Console.WriteLine("Введите начальное число диапозона для заполнения массива NumberStart:");
+int NumberStart = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите конечное число диапозона для заполнения массива NumberEnd:");
+int NumberEnd = int.Parse(Console.ReadLine());
+
+int [] array = new int[8];
+int[] myarray = GetRandome(array, NumberStart, NumberEnd);
+
+GetWriteArray(myarray);
